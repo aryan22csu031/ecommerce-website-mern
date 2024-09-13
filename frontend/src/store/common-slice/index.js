1fc1c91d38dd71bdbd8db14b576a70153c0f2f6a
aryan22csu031 <aryan22csu031@ncuindia.edu>
2024-09-13 21:32:33 +0530

    changes

--- frontend/src/store/common-slice/index.js
+++ frontend/src/store/common-slice/index.js
@@ -7,7 +7,7 @@ import img4 from "./img/img4.jpeg";
 
 const initialState = {
   isLoading: false,
-  featureImageList: [{img1}, {img2}, {img3}, {img4}],
+  featureImageList: [img1, img2, img3, img4],
 };
 
 export const getFeatureImages = createAsyncThunk(
